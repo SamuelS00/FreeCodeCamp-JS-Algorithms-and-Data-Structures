452adfd3e1fc7a1b7ad29af65fe07803f25c3a74
Samuel <samuelsoutosilva14@gmail.com>
2022-09-21 22:14:17 -0300

    chore: remove only

--- IntermediateAlgorithmScripting/dna-pair/ex.test.ts
+++ IntermediateAlgorithmScripting/dna-pair/ex.test.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 
 import pairElement from './ex';
 
-describe.only('test DNA pairing', () => {
+describe('test DNA pairing', () => {
   it('pairElement("ATCGA") should return [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]]', () => {
     expect(pairElement("ATCGA")).to.be.deep.equal([["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]]);
   });
