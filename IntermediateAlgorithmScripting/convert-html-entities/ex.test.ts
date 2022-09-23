7e7065a02f8cc5cc50f68dd88638c541d094a836
Samuel <samuelsoutosilva14@gmail.com>
2022-09-23 11:58:26 -0300

    test: add test for convertHTML function

---
+++ IntermediateAlgorithmScripting/convert-html-entities/ex.test.ts
@@ -0,0 +1,32 @@
+import mocha from 'mocha';
+import { expect } from 'chai';
+
+describe('test convertHTML function', () => {
+  it('convertHTML("Dolce & Gabbana") should return the string Dolce &amp; Gabbana.', () => {
+    expect(convertHTML("Dolce & Gabbana")).to.be.equal('Dolce &amp; Gabbana');
+  });
+
+  it('convertHTML("Hamburgers < Pizza < Tacos") should return the string Hamburgers &lt; Pizza &lt; Tacos', () => {
+    expect(convertHTML("Hamburgers < Pizza < Tacos")).to.be.equal('Hamburgers &lt; Pizza &lt; Tacos');
+  });
+ 
+  it('convertHTML("Sixty > twelve") should return the string Sixty &gt; twelve', () => {
+    expect(convertHTML("Sixty > twelve")).to.be.equal('Sixty &gt; twelve');
+  });
+
+  it('Waiting:convertHTML(\'/Stuff in "quotation marks"\') should return the string Stuff in &quot;quotation marks&quot', () => {
+    expect(convertHTML('Stuff in "quotation marks"')).to.be.equal('Stuff in &quot;quotation marks&quot')
+  });
+
+  it('convertHTML("Schindler\'s List") should return the string Schindler&apos;s List', () => {
+    expect(convertHTML("Schindler's List")).to.be.equal('Schindler&apos;s List')
+  });
+
+  it('convertHTML("<>") should return the string &lt;&gt;', () => {
+    expect(convertHTML("<>")).to.be.equal('&lt;&gt;')
+  });
+
+  it('convertHTML("abc") should return the string abc', () => {
+    expect(convertHTML("abc")).to.be.equal('abc')
+  });
+});
