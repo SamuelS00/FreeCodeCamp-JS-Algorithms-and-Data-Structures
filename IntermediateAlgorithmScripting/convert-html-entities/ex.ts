0d10a8f35ddcf4d8dbc5ccefa1f0d7044a269a90
Samuel <samuelsoutosilva14@gmail.com>
2022-09-23 13:46:21 -0300

    feat: add function convertHTML

---
+++ IntermediateAlgorithmScripting/convert-html-entities/ex.ts
@@ -0,0 +1,39 @@
+type HtmlEntities = {
+  "&"?: string,
+  "<"?: string,
+  ">"?: string,
+  '"'?: string,
+  "'"?: string,
+}
+
+function getValues<T, R extends keyof T>(obj: T, propertyName: R) {
+  return obj[propertyName];
+};
+
+// getValues<propertyName, keyof propertyName>(obj, key as keyof propertyName);
+
+function convertHTML(str: string) {
+    const htmlEntities: HtmlEntities = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': "&quot;",
+        "'": "&apos;"
+    };
+
+    const arrStr = str.split('');
+
+    const newArr = arrStr.map((c) => {
+      let isHtmlEntities = getValues(htmlEntities, c as keyof HtmlEntities);
+
+      if (isHtmlEntities) return isHtmlEntities;
+
+      return c;
+    });
+
+    const newStr = newArr.join('');
+
+    return newStr;
+};
+
+export default convertHTML;
