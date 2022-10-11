5056e1c34473c2306ce4752d7143a3636f63800c
Samuel <samuelsoutosilva14@gmail.com>
2022-10-10 22:14:09 -0300

    feat: add function results

---
+++ Algorithms-and-Data-Structures-Projects/cash-register/results.ts
@@ -0,0 +1,89 @@
+import checkCashRegister from "./ex";
+
+const result1 = checkCashRegister(
+  19.5,
+  20,
+  [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100]
+  ]
+);
+
+const result3 = checkCashRegister(
+  3.26,
+  100,
+  [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100]
+  ]
+);
+
+const result4 = checkCashRegister(
+  19.5,
+  20,
+  [
+    ["PENNY", 0.01],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
+  ]
+);
+
+const result5 = checkCashRegister(
+  19.5,
+  20,
+  [
+    ["PENNY", 0.01],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 1],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
+  ]
+);
+
+const result6 = checkCashRegister(
+  19.5,
+  20,
+  [
+    ["PENNY", 0.5],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
+  ]
+);
+
+export {
+  result1,
+  result3,
+  result4,
+  result5,
+  result6,
+}
